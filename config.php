fd32d85f966595d88b9a64d26346f82aeeeb63bb
jorn <jornschalkwijk@gmail.com>
2016-10-28 13:06:10 +0200

    mac config changes

--- config.php
+++ config.php
@@ -9,21 +9,21 @@
 //	define ('DB_NAME', 'cms');
 //	define ('HOST','http://'.$_SERVER['HTTP_HOST']);
 	// Mac
-	// define ('IMG_PATH','/images/');
-	// define ('FILE_PATH', '/files/');
-	// define ('DB_HOST','localhost');
-	// define ('DB_USER','root');
-	// define ('DB_PASSWORD', 'root');
-	// define ('DB_NAME', 'cms');
-	// define ('HOST','http://'.$_SERVER['HTTP_HOST']);
-	// Windows
-	 define ('IMG_PATH','/cms/images/');
-	 define ('FILE_PATH', '/cms/files/');
+	 define ('IMG_PATH','/images/');
+	 define ('FILE_PATH', '/files/');
 	 define ('DB_HOST','localhost');
 	 define ('DB_USER','root');
-	 define ('DB_PASSWORD', '');
+	 define ('DB_PASSWORD', 'root');
 	 define ('DB_NAME', 'nerdcms_db');
-	 define ('HOST','http://'.$_SERVER['HTTP_HOST'].'/cms');
+	 define ('HOST','http://'.$_SERVER['HTTP_HOST']);
+//	// Windows
+//define ('IMG_PATH','/cms/images/');
+//define ('FILE_PATH', '/cms/files/');
+//define ('DB_HOST','localhost');
+//define ('DB_USER','root');
+//define ('DB_PASSWORD', '');
+//define ('DB_NAME', 'nerdcms_db');
+//define ('HOST','http://'.$_SERVER['HTTP_HOST'].'/cms');
 
 // Composer autoloader;
 require_once(__DIR__. '/admin/vendor/autoload.php');
