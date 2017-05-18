4da20f16b7d6db248b3639fa43b640ae446b5f83
jorn <jornschalkwijk@gmail.com>
2017-05-18 19:10:41 +0200

    mistake is view/add-files.php, find out whats going on

--- admin/view/files/add-files.php
+++ admin/view/files/add-files.php
@@ -13,7 +13,7 @@ use CMS\Models\Files\Folders;
 <!--<div class="container">-->
 <!--	<div class="row">-->
 <!--		<div class="col-sm-6 col-lg-6 col-sm-offset-3 push-lg-3">-->
-			<form class="small" enctype="multipart/form-data" method="post" action="<?php (!isset($params[0git stat])) ? ADMIN."files" : ADMIN."files/albums/".$params[0].'/'.$params[1];?>">
+			<form class="small" enctype="multipart/form-data" method="post" action="<?php (!isset($params[0])) ? ADMIN."files" : ADMIN."files/albums/".$params[0].'/'.$params[1];?>">
 				<input type="hidden" name="MAX_FILE_SIZE" value="43500000" />
 				<label for="files[]">Choose File(max size: 3.5 MB): </label><br />
 				<input type="file" name="files[]" multiple/><br />
